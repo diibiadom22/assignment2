be3b012a8cdc0f9d7c067a047908210023a3dbec
diibiadom22 <odiibiadom@gmail.com>
2024-09-10 05:16:48 -0700

    first commit

---
+++ myindex.js
@@ -0,0 +1,90 @@
+
+//   let x=20;
+// let y=14;
+//  console.log('x++ = ', x++);
+// console.log('++x =',++x);
+// console.log('++x =',++x);
+// console.log('x++ =',x++);
+// console.log(2 == '2');
+// let _abc = "abc";
+// console.log(_abc);
+// const age = 18;
+// if (age <= 18) {
+//  console.log("Nick is an adult.");
+// }
+// else {
+//    console.log("Nick is a child.");
+// }
+
+// const diiAge = 4;
+// if (diiAge < 4) {
+//    console.log("Alice is under 18 years old.");
+// } else if (diiAge >= 4 && diiAge >= 21) {
+//   console.log("Alice is between the ages of 18 and 21.");
+// } else {
+//   console.log("Alice is over 21 years old.");
+// }
+
+//  const pet = "dog";
+
+// if (pet === "lizard") {
+//    console.log("I own a lizard");
+// } else if (pet === "dog") {
+//    console.log("I own a dog");
+// } else if (pet === "cat") {
+//    console.log("I own a cat");
+// } else if (pet === "snake") {
+//    console.log("I own a snake");
+// } else if (pet === "parrot") {
+//    console.log("I own a parrot");
+// } else {
+//    console.log("I don't own a pet");
+// }
+
+// const boyfriendIsPaying = true;
+// const isSale = false;
+// if (!boyfriendIsPaying || isSale) {
+//    console.log("Jesse will go shopping.");
+// } else {
+//    console.log("Jesse will not go shopping.");
+// }
+
+ //program to display the sum of natural numbers. i = 1 to n  in each iteration, i is increased by 1
+ //{ sum += i;  sum = sum + i } console.log('sum:', sum);
+   // let sum = 0; const n = 100 ; let i= 1 ;
+   // for ( i = 1; i <= n; i++) {
+   //  sum += i;
+   //   console.log('sum:', sum);
+   // }
+
+   //program to display the sum of natural numbers.  i = 1 to n  in each iteration, i is increased by 1
+ //{ sum += i;  sum = sum + i } console.log('sum:', sum);
+//  let sumOf = 0; const b = 100 ; let a= 1 ;
+//  for ( a = 1; a <= b; a++) {
+//    sumOf =+a;
+//    console.log('sumOf:', sumOf);
+//  }
+
+ // program to display text 5 times
+// looping from i = 1 to 5
+//   let word=0; const w=5;
+// for (let i = 1; i <= w; i++) {
+//  word = +i;
+//     console.log(`I love JavaScript.`, i);
+// }
+
+// const year = 17; 
+// const ownsCar = false; 
+// if (year >= 16 || ownsCar) {    console.log("Jerry is old enough to drive and has his own car."); 
+// } else { 
+// console.log("Jerry does not drive."); 
+// }
+
+// program to display numbers from 1 to 5
+ // initialize the variable let i = 1, n = 5;
+  // while loop from i = 1 to 5 
+  //     let p = 0; let num = 5;  let i =1;
+  //    while (i <= num) {
+  //     p = +i;
+  //      console.log(p);
+  //  }
